eb6eb818d56941cd38053607b8e98a1fbfbe33ac
Jasmin Miftari <jasmin.mif@gmail.com>
2023-08-25 11:46:36 -0400

    Fix prompt message color

--- utils.sh
+++ utils.sh
@@ -1,8 +1,13 @@
+bold=`tput bold`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
+
 prompt_and_execute() {
   local prompt_message="$1"
   local function_to_run="$2"
   
-  read -p "$prompt_message (y/n): " choice
+  read -p "${green}$prompt_message (y/n): ${reset}" choice
 
   if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
     "$function_to_run"
@@ -15,6 +20,3 @@ print_test_msg() {
   echo "****Test msg****"
 }
 
-bold=$(tput bold)
-yellow=$(tput setaf 3)
-reset=$(tput sgr0)
